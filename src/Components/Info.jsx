485bd88e604b772c11c766d353873016a34f85ff
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-01 22:47:12 +0530

    dark mode

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -1,5 +1,25 @@
+
 const Info = () => {
-   return( <p>Welcome to your third compnents of your react app </p>
-   )
-}
+  const toggleMode = () => {
+    var root = document.getElementById("test");
+    var currentColor = window.getComputedStyle(root).backgroundColor;
+
+    if (currentColor === 'rgb(0, 0, 0)' || currentColor === 'black') {
+      root.style.backgroundColor = "white";
+    } else {
+      root.style.backgroundColor = "black";
+    }
+  };
+
+  return (
+    <>
+      <button  className="toggle-button" onClick={toggleMode}>
+        CHange Mode
+      </button>
+    </>
+  );
+};
+
 export default Info;
+
+
