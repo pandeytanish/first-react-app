65947eba4c5648ddb47724640bd75c65927fa1ff
Tanish Pandey <tanishpandey14@gmail.com>
2024-03-13 20:33:47 +0530

    useRef hook

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import Fun from './Components/Fun';
 import Api from './Components/Api';
 import ColorPicker from './Components/ColorPicker';
 
+import Test from './Components/Test';
+
 function App() {
   return (
 
 <>
-<h1>First React App</h1>
+{/* <h1>First React App</h1>
 < Home />
 <Event/>
 <Info/>
@@ -19,7 +21,8 @@ function App() {
 <Fun/>
 <Api/>
 <ColorPicker/>
-<h1>React App</h1>
+<h1>React App</h1> */}
+<Test/>
 </>
 
   );
